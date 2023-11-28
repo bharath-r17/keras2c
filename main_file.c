de0941f19880b2604903460cca96511aedbcbf41
bharath-r17 <76158612+bharath-r17@users.noreply.github.com>
2023-11-28 12:44:52 +0000

    new main_file with user input option

--- main_file.c
+++ main_file.c
@@ -1,27 +1,37 @@
 #include <stdio.h>
 #include "./include/k2c_include.h"
 #include "test_func.h" // Replace with the actual name of your generated header file
-
-// Function to find the index of the maximum value in an array (not used in this case)
-int argmax(float array[], int size) {
-    // This function is not needed for regression
-    return -1;
-}
-
+ 
 int main() {
-    // Prepare your input data (replace with actual values)
-    float input_data[8] = {-0.35139464349045885, 1.8561815225324745, 0.019648332203345185, -0.03602697199409447, -0.45519335442361386, -0.07769971749617115, 1.038502685950199, -1.3428091431408828};
-    k2c_tensor input = {input_data,1,8,{8,1,1,1,1}}; // Adjust the size to match your model input
-
     // Create an output tensor
     float output_data[1]; // Regression output, so only one value
-    k2c_tensor output = {output_data, 1,1,{1,1,1,1,1}}; // Adjust the size to match your model output
-
-    // Call the generated C function
-    test_func(&input, &output); // Replace with the actual name of your generated function
-
-    // Use the output
-    printf("The model predicts the house price is: %f\n", output_data[0]);
-
+    k2c_tensor output = {output_data, 1, 1, {1, 1, 1, 1, 1}}; // Adjust the size to match your model output
+ 
+    while (1) {
+        // Prepare your input data (replace with actual values)
+        float input_data[8];
+        // Get input from the user
+        printf("Enter 8 values for input separated by spaces: ");
+        for (int i = 0; i < 8; ++i) {
+            scanf("%f", &input_data[i]);
+        }
+ 
+        // Create an input tensor
+        k2c_tensor input = {input_data, 1, 8, {8, 1, 1, 1, 1}}; // Adjust the size to match your model input
+ 
+        // Call the generated C function
+        test_func(&input, &output); // Replace with the actual name of your generated function
+ 
+        // Use the output
+        printf("The model predicts the output is: %f\n", output_data[0]);
+        // Ask the user if they want to continue
+        printf("Do you want to continue? (y/n): ");
+        char choice;
+        scanf(" %c", &choice);
+        if (choice != 'y' && choice != 'Y') {
+            break; // Exit the loop if the user doesn't want to continue
+        }
+    }
+ 
     return 0;
 }
